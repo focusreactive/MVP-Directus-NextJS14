87f693f00bad46bf9cd0602634957b78d9608977
Oleg Proskurin <regx@usul.su>
2023-11-02 22:58:37 +0700

    FIX API

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -1,11 +1,9 @@
-// @ts-ignore
-
 const REVIEW_FLOW =
   'https://artcryption-poc.directus.app/flows/trigger/fb6de8f5-95fe-4aed-a06d-0a6071e98278';
 
 export async function POST(request: Request) {
   const data = await request.json();
-  const payload = JSON.stringify(data.payload);
+  const payload = JSON.parse(data.payload);
   const actions = payload.actions;
   const value = actions[0].value;
 
