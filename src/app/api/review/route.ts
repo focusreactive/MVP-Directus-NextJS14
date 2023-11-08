00c6449c8e0235d797f6821e50dea4dc32dc9eb4
Oleg Proskurin <regx@usul.su>
2023-11-08 20:43:06 +0700

    Update revalidation

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -1,12 +1,14 @@
+export const revalidate = 10;
+
 const REVIEW_FLOW =
   'https://artcryption-poc.directus.app/flows/trigger/fb6de8f5-95fe-4aed-a06d-0a6071e98278';
 
 export async function POST(request: Request) {
-  console.log("🚀 ~ file: route.ts:5 ~ POST ~ request:", request)
+  console.log('🚀 ~ file: route.ts:5 ~ POST ~ request:', request);
   const data = await request.json();
-  console.log("🚀 ~ file: route.ts:6 ~ POST ~ data:", data)
+  console.log('🚀 ~ file: route.ts:6 ~ POST ~ data:', data);
   const payload = JSON.parse(data.payload);
-  console.log("🚀 ~ file: route.ts:8 ~ POST ~ payload:", payload)
+  console.log('🚀 ~ file: route.ts:8 ~ POST ~ payload:', payload);
   const actions = payload.actions;
   const value = actions[0].value;
 
